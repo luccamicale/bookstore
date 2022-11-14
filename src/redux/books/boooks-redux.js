9719fe66929ab9ca9552ec7b19e95b6f6e522db4
luccamicale <luccamicale2015@gmail.com>
2022-11-14 18:00:57 -0300

    add the store and complete

--- src/redux/books/boooks-redux.js
+++ src/redux/books/boooks-redux.js
@@ -1,13 +1,27 @@
+const ADD_BOOK = 'bookstore/src/redux/books/addBook';
+const REMOVE_BOOK = 'bookstore/src/redux/books/removeBook';
 
-
+// action to add
 const addBook = () => ({
-  type: 'ADD-BOOK',
-  payload: add,
+  type: ADD_BOOK,
+  payload: test,
 });
 
+// action to delete
 const removeBook = () => ({
-    type: 'REMOVE-BOOK',
-    payload: quit,
+  type: REMOVE_BOOK,
+  payload: test,
 });
 
-export default { addBook, removeBook };
+// create a reducer
+const initialState = (state = '', action) => {
+  if (action.type === ADD_BOOK) {
+    return action.payload;
+  }
+  if (action.type === REMOVE_BOOK) {
+    return action.payload;
+  }
+  return state; //always is necessary to return the state
+};
+
+export default { addBook, removeBook, initialState };
